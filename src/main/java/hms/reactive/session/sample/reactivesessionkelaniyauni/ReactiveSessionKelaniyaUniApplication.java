5222e788dfba33b65a951a1bcd165eec0a80d8dd
AyeshAlmeida <ayesh9206@gmail.com>
2017-12-01 12:41:10 +0530

    Fix: Initial Project Structure.

---
+++ src/main/java/hms/reactive/session/sample/reactivesessionkelaniyauni/ReactiveSessionKelaniyaUniApplication.java
@@ -0,0 +1,12 @@
+package hms.reactive.session.sample.reactivesessionkelaniyauni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSessionKelaniyaUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSessionKelaniyaUniApplication.class, args);
+	}
+}
