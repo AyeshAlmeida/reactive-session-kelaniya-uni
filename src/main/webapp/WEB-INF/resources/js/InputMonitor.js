a2f7265fb9c2d0c797c1a46c165c315c064761c8
AyeshAlmeida <ayesh9206@gmail.com>
2017-12-02 07:54:40 +0530

    Fix: Minor Refactoring.

--- src/main/webapp/WEB-INF/resources/js/InputMonitor.js
+++ src/main/webapp/WEB-INF/resources/js/InputMonitor.js
@@ -1,44 +1,41 @@
 $(document).ready(function () {
-    Rx.Observable.fromEvent(document.getElementById("author-name"), 'keyup')
-	             .flatMap((e) => (concatObservables(doGetApps(e.target.value), doGetBooks(e.target.value))))
-		         .subscribe((e) => getDataOut(e));
+    Rx.Observable.fromEvent(document.getElementById("author-name"), "keyup")
+                 .flatMap((e) => (concatObservables(doGetApps(e.target.value), doGetBooks(e.target.value))))
+                 .subscribe((e) => insertDataIntoTable(e));
 
 	function doGetApps(e) {
-		var url = "http://localhost:7878/application?author=";
-		url += e;
-		return Rx.Observable.fromPromise($.getJSON(url));
+        var url = "http://localhost:7878/application?author="+e;
+        return Rx.Observable.fromPromise($.getJSON(url));
 	}
 
 	function doGetBooks(e) {
-		var url = "http://localhost:7878/book?author=";
-		url += e;
-		return Rx.Observable.fromPromise($.getJSON(url));
-	}
+        var url = "http://localhost:7878/book?author="+e;
+        return Rx.Observable.fromPromise($.getJSON(url));
+    }
 
 	function concatObservables(ob1, ob2) {
 		return ob1.merge(ob2).reduce((acc, elem) => acc.concat(elem), []);
 	}
 
-	function getDataOut(e) {
-		var table = document.getElementById('data-table');
-		var tbody = table.getElementsByTagName('tbody')[0];
-		deleteTableRows(tbody);
-		for (var i =0; i < e.length; i++){
-			var element = e[i];
-			var row = tbody.insertRow(i);
-			var cell1 = row.insertCell(0);
-			var cell2 = row.insertCell(1);
-			var cell3 = row.insertCell(2);
-			cell1.innerHTML = element.name;
-			cell2.innerHTML = element.author;
-			cell3.innerHTML = element.type;
-		}
+	function insertDataIntoTable(data_set) {
+		var table = document.getElementById("data-table");
+		var table_body = table.getElementsByTagName("tbody")[0];
+		deleteTableRows(table_body);
+        data_set.forEach(function (element, index) {
+            var row = table_body.insertRow(index);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            cell1.innerHTML = element.name;
+            cell2.innerHTML = element.author;
+            cell3.innerHTML = element.type;
+        });
 	}
 
 	function deleteTableRows(tableBody) {
-		while (tableBody.hasChildNodes()) {
-			tableBody.removeChild(tableBody.lastChild);
-		}
-	}
+        while (tableBody.hasChildNodes()) {
+            tableBody.removeChild(tableBody.lastChild);
+        }
+    }
 });
 
